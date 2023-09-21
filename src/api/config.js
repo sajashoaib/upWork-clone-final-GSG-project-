d710f3ed52bca1977fff2537b724967c153cb314
sajashoaib <islamsaja08@gmail.com>
2023-09-21 19:08:54 +0300

    updating

--- src/api/config.js
+++ src/api/config.js
@@ -6,3 +6,8 @@ export const myAxios=axios.create({
 export const addTokenToTheAxios=(token)=>{
     myAxios.defaults.headers['Authorization']=`Bearer ${token}`;  
 }
+
+export const AUTH_API = process.env.REACT_APP_AUTH_API;
+if (process.env.NODE_ENV === "production") {
+  console.log("production");
+}
