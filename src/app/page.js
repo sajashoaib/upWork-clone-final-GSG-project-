7d98245ee5986768eb43adcee883a5218f65ad1f
sajashoaib <islamsaja08@gmail.com>
2023-09-21 18:22:06 +0300

    update page.js by remove unnessasary import

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,11 @@
-import Image from 'next/image'
+'use client'
 import styles from './page.module.css'
-import Typography from '@/components/atoms/Typography'
-import NavBar from '@/components/molecules/NavBar'
+import Login from '@/components/pages/Login'
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
-<NavBar/>
-<Typography as='h1'>hii</Typography>
-<Typography as='h2'>hiii</Typography>
-<Typography as='h3'>hiii</Typography>
-<Typography as='h4'>hiii</Typography>
-<Typography as='h5'>hiii</Typography>
-<Typography as='h6'>hiii</Typography>
-<Typography as='p'>hiii</Typography>
-
-
-
-
+<Login/>
     </main>
   )
 }
