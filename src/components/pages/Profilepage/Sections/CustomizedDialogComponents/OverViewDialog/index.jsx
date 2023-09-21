74bdadc10b89cbbcf75eb0dd408cac1f3433c299
sajashoaib <islamsaja08@gmail.com>
2023-09-21 18:27:12 +0300

    update OverViewDialog

--- src/components/pages/Profilepage/Sections/CustomizedDialogComponents/OverViewDialog/index.jsx
+++ src/components/pages/Profilepage/Sections/CustomizedDialogComponents/OverViewDialog/index.jsx
@@ -1,13 +1,19 @@
 import Typography from '@/components/atoms/Typography'
 import { StyledLinks } from '@/components/pages/Homepage/sections/DropDownList/style';
-import React from 'react'
+import { Button } from '@mui/material';
+import React,{useState} from 'react'
 
-const OverViewDialog = () => {
-    const items = ['Describe your strengths and skills',
+const OverViewDialog = ({ text, onSaveChanges }) => {
+    const items = [
+      'Describe your strengths and skills',
      'Highlight projects, accomplishments and education',
      'Keep it short and make sure it is error-free',
      ];
+     const [editedText, setEditedText] = useState(text);
 
+     const handleSaveChanges = () => {
+       onSaveChanges(editedText);
+     };
   return (
     <div>
       <Typography as='p'>Use this space to show clients you have the skills and experience they are looking for.</Typography>
@@ -22,7 +28,12 @@ const OverViewDialog = () => {
         cols="50" 
         style={{width:'100%'}}
         placeholder="Enter your text here..." 
+        value={editedText}
+        onChange={(e) => setEditedText(e.target.value)}
       />
+        <Button autoFocus onClick={handleSaveChanges}>
+        Save changes
+      </Button>
     </div>
   )
 }
