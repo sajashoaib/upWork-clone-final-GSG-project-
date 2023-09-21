a6f62646471eadb1baad57a5ad76ce277a0a68f0
sajashoaib <islamsaja08@gmail.com>
2023-09-22 00:38:23 +0300

    fix bugs : Hydration error by remove previous MUI Tab and Add new one from library

--- src/components/pages/Homepage/sections/Tabs/index.jsx
+++ src/components/pages/Homepage/sections/Tabs/index.jsx
@@ -1,79 +1,32 @@
-import React, { useEffect ,useState} from "react";
-import Tabs from "@mui/material/Tabs";
-import Box from "@mui/material/Box";
-import PropTypes from "prop-types";
-import { StyledTab } from "./style";
-import Typography from "@/components/atoms/Typography";
-import Jobs from "../Jobs";
-import SavedJobs from "../SavedJobs";
-import EachJob from "../Jobs/EachJob";
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Jobs from '../Jobs';
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-const TabsJob = ({ savedJobs }) => {
-  const [value, setValue] = React.useState(0);
-  useEffect(() => {
-    
-  }, []);
+export default function TabsJob() {
+  const [value, setValue] = React.useState('1');
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <StyledTab label="Best Matches" {...a11yProps(0)} />
-          <StyledTab label="Most Recent" {...a11yProps(1)} />
-          <StyledTab label="Saved Jobs" {...a11yProps(2)} />
-        </Tabs>
-      </Box>
-      <CustomTabPanel value={value} index={0}>
-         <Jobs /> 
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        Item Two
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <SavedJobs />
 
-      </CustomTabPanel>
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Best Matches" value="1" />
+            <Tab label="Most Recent" value="2" />
+            <Tab label="Saved Jobs" value="3" />
+          </TabList>
+        </Box>
+        <TabPanel value="1"><Jobs/></TabPanel>
+        <TabPanel value="2">Item Two</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+      </TabContext>
     </Box>
   );
-};
-
-export default TabsJob;
+}
