3abddb0f18df109d8c3f6f08b5f8246c75be961a
sajashoaib <islamsaja08@gmail.com>
2023-09-21 18:26:22 +0300

    create Signup index

--- src/components/pages/SignUp/index.jsx
+++ src/components/pages/SignUp/index.jsx
@@ -1,29 +1,27 @@
-'use client'
-import React from 'react'
-import NavLoginSignUp from '@/components/molecules/NavBarLogin&SignUp'
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-import { StyledBox } from '../Login/style';
-import SignUpForm from './SignUPForm';
-import FooterLoginSignUp from '@/components/molecules/FooterLogin&Signup';
-
-
+"use client";
+import React from "react";
+import NavLoginSignUp from "@/components/molecules/NavBarLogin&SignUp";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { StyledBox } from "../Login/style";
+import SignUpForm from "./SignUPForm";
+import FooterLoginSignUp from "@/components/molecules/FooterLogin&Signup";
 
 const SignUp = () => {
   return (
     <div>
-      <NavLoginSignUp isLoginPage={true}/>
+      <NavLoginSignUp isLoginPage={true} />
       <React.Fragment>
-    <CssBaseline />
-    <Container maxWidth="sm"  >
-      <StyledBox>
-        <SignUpForm/>
-      </StyledBox>     
-    </Container>
-  </React.Fragment>
-  <FooterLoginSignUp/>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <StyledBox>
+            <SignUpForm />
+          </StyledBox>
+        </Container>
+      </React.Fragment>
+      <FooterLoginSignUp />
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
