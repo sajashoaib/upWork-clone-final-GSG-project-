fe9868beed9921afa716efdff2b4eeb38f14459a
sajashoaib <islamsaja08@gmail.com>
2023-09-21 15:56:49 +0300

    create files in constant folder

---
+++ src/constant/paths.js
@@ -0,0 +1,6 @@
+export const PATHS = {
+  "HOME" : '/home',
+   "SIGNUP" : '/signup',
+  "LOGIN" : '/login',
+
+}
--- src/constant/theme.js
+++ src/constant/theme.js
@@ -1,24 +1,12 @@
-'use client'
+"use client";
 
 import { fontSizes, fonts, lightColors, media } from "./style";
 
-const { createTheme} = require("@mui/material");
+const { createTheme } = require("@mui/material");
 
- export const theme= createTheme({
-    typography:fonts,
-    colors:lightColors,
-    fontsize:fontSizes,
-    media:media
-
-})
-/*
-    margin: 0;
-    font-family: var(--font-family-base),"Helvetica Neue",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-    font-size: var(--font-size-base);
-    line-height: 1.428571429;
-    letter-spacing: var(--letter-spacing-base);
-    color: var(--text-dark);
-    background-color: var(--body-bg);
-
-
-*/ 
+export const theme = createTheme({
+  typography: fonts,
+  colors: lightColors,
+  fontsize: fontSizes,
+  media: media,
+});
