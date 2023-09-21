da6f8283fc464f710b3da7b3ac91e8467b10adc1
sajashoaib <islamsaja08@gmail.com>
2023-09-21 15:59:32 +0300

    create Jobs folder

--- src/components/pages/Homepage/sections/Jobs/index.jsx
+++ src/components/pages/Homepage/sections/Jobs/index.jsx
@@ -1,17 +1,28 @@
+'use client'
 import Typography from '@/components/atoms/Typography'
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { StyledJobs, StyledTitleJob } from './style'
 import EachJob from './EachJob'
 import { EachJobData } from '@/mock/EachJobData'
 
 const Jobs = () => {
+  const handleSaveJob = () => {
+    console.log('Job saved');
+  };
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/jobs")
+      .then((response) => response.json())
+      .then((data) => setJobs(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+   
   return (
     <StyledJobs>
       <Typography as='p'>Browse jobs that match your experience to a clients hiring preferences. Ordered by most relevant.</Typography>
-      {EachJobData.map((job,index) => (
-  
-        <EachJob key={index} {...job} />
-
+      {jobs.map((job,index) => (
+          <EachJob key={index} {...job} data={job} onSaveJob={handleSaveJob}/>
       ))}
     </StyledJobs>
   )
--- src/components/pages/Homepage/sections/Jobs/style.js
+++ src/components/pages/Homepage/sections/Jobs/style.js
@@ -2,8 +2,6 @@ import Typography from "@/components/atoms/Typography";
 import { styled } from "@mui/material";
 
 export const StyledJobs=styled('div')(({theme})=>({
-    borderTop: `2px solid ${theme.colors.gray03}`, 
-    // padding:'0 20px'
   
 }))
 export const StyledTitleJob = styled(Typography)(({ theme }) => ({
