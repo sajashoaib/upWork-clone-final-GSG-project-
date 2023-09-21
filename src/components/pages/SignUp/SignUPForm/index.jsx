6e44beab1c9bfe0724f8be296cb197946674ee3c
sajashoaib <islamsaja08@gmail.com>
2023-09-21 18:34:06 +0300

    update SignupForm

--- src/components/pages/SignUp/SignUPForm/index.jsx
+++ src/components/pages/SignUp/SignUPForm/index.jsx
@@ -1,12 +1,12 @@
 import Typography from "@/components/atoms/Typography";
 import React from "react";
 import OrLine from "@/components/atoms/OrLine";
-import Buttons from "@/components/atoms/Button";
 import ButtonWithIcon from "@/components/atoms/ButtonWithIcon";
 import { StyledDiv } from "../../Login/LoginForm/style";
 import { ButtonWithIconMock2 } from "@/mock/ButtonWithIconMaock2";
 import { StyledSpan } from "@/components/molecules/NavBarLogin&SignUp/style";
 import SignupFormInputs from "../SignupFormInputs";
+import { StyledLinks } from "../../Homepage/sections/DropDownList/style";
 
 const SignUpForm = () => {
   return (
@@ -23,19 +23,13 @@ const SignUpForm = () => {
           onClick={() => console.log(`Button ${index + 1} clicked`)}
         />
       ))}
-      <OrLine width ="95%">Or</OrLine>
-      <SignupFormInputs/>
-
-      <Buttons
-        variant="outlined"
-        label="Create my account"
-        width="530px"
-        backgroundColor="#108a00"
-        textColor="White"
-        border="2px solid #108a00"
-      />
+      <OrLine width="95%">Or</OrLine>
+      <SignupFormInputs />
       <Typography as="p">
-        Already hava an account?<StyledSpan> Log In</StyledSpan>
+        Already hava an account?
+        <StyledLinks href="/login">
+          <StyledSpan> Log In</StyledSpan>
+        </StyledLinks>
       </Typography>
     </StyledDiv>
   );
