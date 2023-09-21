1461910c92b24af177a99885b7af0960d89da960
sajashoaib <islamsaja08@gmail.com>
2023-09-21 18:32:36 +0300

    update Tabs

--- src/components/pages/Homepage/sections/Tabs/index.jsx
+++ src/components/pages/Homepage/sections/Tabs/index.jsx
@@ -1,29 +1,79 @@
-import React from 'react'
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
-import { StyledTab } from './style';
+import React, { useEffect ,useState} from "react";
+import Tabs from "@mui/material/Tabs";
+import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
+import { StyledTab } from "./style";
+import Typography from "@/components/atoms/Typography";
+import Jobs from "../Jobs";
+import SavedJobs from "../SavedJobs";
+import EachJob from "../Jobs/EachJob";
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-const TabsJob = () => {
-    const [value, setValue] = React.useState('one');
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const TabsJob = ({ savedJobs }) => {
+  const [value, setValue] = React.useState(0);
+  useEffect(() => {
+    
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
   return (
-    <Box sx={{ width: '100%' }}>
-    <Tabs
-      value={value}
-      onChange={handleChange}
-      indicatorColor="primary"
-      aria-label="secondary tabs example"
-    >
-      <StyledTab value="one" label="Best Matches" />
-      <StyledTab  value="two" label="Most Recent" />
-      <StyledTab  value="three" label="Saved Jobs" />
-    </Tabs>
-  </Box>
-  )
-}
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <StyledTab label="Best Matches" {...a11yProps(0)} />
+          <StyledTab label="Most Recent" {...a11yProps(1)} />
+          <StyledTab label="Saved Jobs" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+         <Jobs /> 
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <SavedJobs />
 
-export default TabsJob
+      </CustomTabPanel>
+    </Box>
+  );
+};
+
+export default TabsJob;
