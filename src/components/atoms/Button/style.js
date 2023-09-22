1a08fc1a0d5255bd35a66f39ace383a3a3ac5b02
sajashoaib <islamsaja08@gmail.com>
2023-09-22 18:11:27 +0300

    update to style.js

--- src/components/atoms/Button/style.js
+++ src/components/atoms/Button/style.js
@@ -1,24 +1,12 @@
 'use client'
 import { Button,  styled } from "@mui/material";
-export const StyledButton=styled(Button)(({theme,width,backgroundColor,textColor,border})=>({
+export const StyledButton=styled(Button)(({theme,width,backgroundcolor,textcolor,border})=>({
     width: width, 
-    backgroundColor: backgroundColor, 
-    color: textColor, 
+    backgroundColor: backgroundcolor, 
+    color: textcolor, 
     borderRadius:'30px',
     border:border,
-    // padding:'20px',
     '&:hover': {
       backgroundColor: '#03d01d', 
     },  
 }))
-/* background-color:${(props)=>props.theme.colors.baseColorGreen}; */
-/* width: 350px; */
-/* border-radius: 20px;
-padding: 10px 20px;
-&:hover {
- background-color:${(props)=>props.theme.colors.baseColorGreen200};
-  transition: background-color 0.5s; */
-
-
-    
-  
