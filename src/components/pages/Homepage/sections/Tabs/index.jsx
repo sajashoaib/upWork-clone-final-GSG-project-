62b7e88fee906c5d2ae2b25a0afd51d445469c62
sajashoaib <islamsaja08@gmail.com>
2023-09-23 02:28:16 +0300

    update TabsJob to add EachJob in tab3

--- src/components/pages/Homepage/sections/Tabs/index.jsx
+++ src/components/pages/Homepage/sections/Tabs/index.jsx
@@ -1,32 +1,47 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import TabList from '@mui/lab/TabList';
-import TabPanel from '@mui/lab/TabPanel';
-import Jobs from '../Jobs';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import Jobs from "../Jobs";
+import { useSavedJobData } from "@/context/SavedJobContext";
+import EachJob from "../Jobs/EachJob";
 
 export default function TabsJob() {
-  const [value, setValue] = React.useState('1');
-
+  const [value, setValue] = React.useState("1");
+  const { savedJobs } = useSavedJobData();
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100%', typography: 'body1' }}>
+    <Box sx={{ width: "100%", typography: "body1" }}>
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
             <Tab label="Best Matches" value="1" />
             <Tab label="Most Recent" value="2" />
             <Tab label="Saved Jobs" value="3" />
           </TabList>
         </Box>
-        <TabPanel value="1"><Jobs/></TabPanel>
+        <TabPanel value="1">
+          <Jobs />
+        </TabPanel>
         <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="3">
+          {savedJobs.map((savedJob, index) => (
+            <EachJob
+              key={index}
+              id={savedJob.id}
+              isSelected={true}
+              toggleSelection={() => toggleSelection(savedJob)}
+              {...savedJob}
+            />
+          ))}
+        </TabPanel>
       </TabContext>
     </Box>
   );
 }
+
