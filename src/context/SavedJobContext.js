0dd319c836d7dfb1956cd84d871086307e870c30
sajashoaib <islamsaja08@gmail.com>
2023-09-24 13:24:41 +0300

    update: when reload page save data

--- src/context/SavedJobContext.js
+++ src/context/SavedJobContext.js
@@ -1,17 +1,28 @@
 'use client'
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
+const LOCAL_STORAGE_KEY = "savedJobs";
 const SavedJobContext = createContext();
 
 export const SavedJobProvider = ({ children }) => {
-  const [savedJobs, setSavedJobs] = useState([]);
+  const [savedJobs, setSavedJobs] = useState(()=>{
+  const savedJobData = localStorage.getItem(LOCAL_STORAGE_KEY);
+  return savedJobData ? JSON.parse(savedJobData) : null;
+  });
+  useEffect(() => {
+    if (savedJobs) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(savedJobs));
+    } else {
+      localStorage.removeItem(LOCAL_STORAGE_KEY);
+    }
+  }, [savedJobs]);
 
   const addSavedJob = (job) => {
     setSavedJobs([...savedJobs, job]);
   };
 
   return (
-    <SavedJobContext.Provider value={{ savedJobs, addSavedJob }}>
+    <SavedJobContext.Provider value={{ savedJobs, addSavedJob}}>
       {children}
     </SavedJobContext.Provider>
   );
